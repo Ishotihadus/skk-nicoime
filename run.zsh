aa9345b6a407145a09c2ac85332e4289a9417cb0
Ishotihadus <webmaster@ishotihadus.com>
2023-10-18 00:26:23 +0900

    Update

--- run.zsh
+++ run.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 
-curl http://tkido.com/nicoime/nicoime_latest.html
-curl http://tkido.com/nicoime/nicoime.zip -o nicoime.zip
+mkdir -p out
+curl https://tkido.com/nicoime/nicoime_latest.html
+curl https://tkido.com/nicoime/nicoime.zip -o nicoime.zip
 7z x -y -onicoime nicoime.zip
 ruby skk_convert.rb
 skkdic-expr2 nicoime/nicoime_skk.txt > out/nicoime_skk.utf8
