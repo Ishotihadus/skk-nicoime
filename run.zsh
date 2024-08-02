b5555afc4055cc3c17d67be519c00471f474cd64
Ishotihadus <5352175+Ishotihadus@users.noreply.github.com>
2024-08-02 20:07:02 +0900

    update

--- run.zsh
+++ run.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-curl http://public.s3.tkido.com.s3-website-ap-northeast-1.amazonaws.com/nicoime_latest.html
-curl http://public.s3.tkido.com.s3-website-ap-northeast-1.amazonaws.com/nicoime.zip -o nicoime.zip
+curl https://tkido.com/nicoime/nicoime_latest.html
+curl https://tkido.com/nicoime/nicoime.zip -o nicoime.zip
 7z x -y -onicoime nicoime.zip
 ruby skk_convert.rb
 skkdic-expr2 nicoime/nicoime_skk.txt > out/nicoime_skk.utf8
